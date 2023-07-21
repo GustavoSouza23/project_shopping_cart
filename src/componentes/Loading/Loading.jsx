aef32c814aac5b1ecf5d99d42eac934d85d93894
Gustavo Souza <gustavo03en@gmail.com>
2023-07-21 11:41:23 -0300

    project_shopping_cart

---
+++ src/componentes/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BiLoader } from 'react-icons/bi';
+
+import './Loading.css';
+
+const Loading = () => {
+  return <BiLoader className="loading" />;
+};
+
+export default Loading;
+
